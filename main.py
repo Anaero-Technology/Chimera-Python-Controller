6b005f6815edf4a96362c1aba51819f397cc6556
Robbie Goldman <goldmanrobbie@anaero.co.uk>
2025-03-26 12:53:09 +0000

    Updated version number

--- main.py
+++ main.py
@@ -2598,7 +2598,7 @@ if __name__ == "__main__":
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
     #Set the title text of the window
-    root.title("Chimera Client V2.3")
+    root.title("Chimera Client V3.0")
     #Add the editor to the root windows
     window = MainWindow(root)
     window.grid(row = 0, column=0, sticky="NESW")
